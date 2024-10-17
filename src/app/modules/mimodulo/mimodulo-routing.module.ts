2d1ebce9f8cf5e14580cc3bf71830254e39c1b5f
Gonzalo <gonzz777@gmail.com>
2024-10-17 20:57:19 -0300

    3

--- src/app/modules/mimodulo/mimodulo-routing.module.ts
+++ src/app/modules/mimodulo/mimodulo-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChoferesAltaComponent } from './components/choferes-alta/choferes-alta.component';
 import { ChoferesComponent } from './components/choferes/choferes.component';
+import { guardGuard } from '../../guards/guard.guard';
 
 
 const routes: Routes = [
@@ -10,7 +11,7 @@ const routes: Routes = [
     path: 'alta', component:ChoferesAltaComponent
   },
   {
-    path: 'choferes', component:ChoferesComponent
+    path: 'choferes', component:ChoferesComponent, canActivate:[guardGuard]
   }
 
 ];
