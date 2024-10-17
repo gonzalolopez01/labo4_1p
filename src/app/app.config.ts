829ac675b6c1f0dc700c951ccc5baed7fd9c1fc7
Gonzalo <gonzz777@gmail.com>
2024-10-17 18:21:34 -0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+  provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes), 
+  provideFirebaseApp(() => initializeApp({"projectId":"simuacro-parcial1","appId":"1:1050263600187:web:8de52d6cf1d40b6307b01e","storageBucket":"simuacro-parcial1.appspot.com","apiKey":"AIzaSyAPz3Xfy1w76awfKuxEy0T-dHz8a6ZScjc","authDomain":"simuacro-parcial1.firebaseapp.com","messagingSenderId":"1050263600187"})), 
+  provideAuth(() => getAuth()), 
+  provideFirestore(() => getFirestore()),
+  provideHttpClient()
+]
 };
---
+++ src/app/modules/mimodulo/mimodulo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MimoduloRoutingModule } from './mimodulo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MimoduloRoutingModule
+  ]
+})
+export class MimoduloModule { }
